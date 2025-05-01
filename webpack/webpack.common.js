5e2133343911812fab0c31ea1283491005cc6372
Khaspalaev Anush <khaspalaevan05@gmail.com>
2025-05-01 22:35:19 +0300

    initialize with webpack

---
+++ webpack/webpack.common.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLWebpackPlugins = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const production = process.env.NODE_ENV === 'production'; 
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, '..', './dist'),
+        filename: production
+            ? 'static/scripts/[name].[contenthash].js'
+            : 'static/scripts/[name].js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    production ? MiniCssExtractPlugin.loader : 'style-loader', 
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                                auto: /\.module\.\w+$/i,
+                            },
+                            importLoaders: 2,
+                        },
+                    },
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|webp)$/,
+                type: 'asset/resource',
+                    generator: {
+                        filename: 'static/images/[hash][ext][query]',
+                    },
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: ['@svgr/webpack', 'url-loader'],
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                    generator: {
+                        filename: 'static/fonts/[hash][ext][query]',
+                    },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
+    },
+    plugins: [
+        new HTMLWebpackPlugins({
+           template: path.resolve(__dirname, 'public/index.html')
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'static/styles/index.css',
+        }),
+    ],
+};
